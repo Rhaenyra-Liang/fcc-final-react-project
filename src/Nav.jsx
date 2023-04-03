4f9cde72f60f7e530b01dbe55e9a1e9cd869dd84
Rhaenyra Liang <sofialiang19991029@gmail.com>
2023-04-03 22:16:14 +0800

    fixed text-color2

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -18,34 +18,34 @@ function Nav() {
       />
       <div
         className={`${
-          isNavbarOpen ? "right-[21%]" : "right-[0%]"
+          isNavbarOpen ? "right-[25%]" : "right-[0%]"
         } h-screen flex gap-4 duration-500 fixed w-16 z-10`}
         // onClick={() => setIsNavbarOpen(!isNavbarOpen)}
       >
-        <nav className="flex items-start fixed ">
-          <ul className="bg-white/20 h-16 backdrop-blur-3xl w-fit px-2 flex gap-x-7 items-center">
+        <nav className="flex items-start fixed text-xl">
+          <ul className=" h-16  w-fit px-2 flex gap-x-7 items-center">
             <li>
               <Link to="/home">
                 {/* <Tooltip placement="bottom" title={text1}></Tooltip> */}
-                <h1>Home</h1>
+                <h1 className=" text-gray-100">Home</h1>
               </Link>
             </li>
             <li>
               <Link to="/quote">
                 {/* <Tooltip placement="bottom" title={text1}></Tooltip> */}
-                <h1>Quote</h1>
+                <h1 className="text-gray-100">Quote</h1>
               </Link>
             </li>
             <li>
               <Link to="/excalidraw">
                 {/* <Tooltip placement="bottom" title={text1}></Tooltip> */}
-                <h1>Excalidraw</h1>
+                <h1 className="text-gray-100">Excalidraw</h1>
               </Link>
             </li>
             <li>
               <Link to="/quote">
                 {/* <Tooltip placement="bottom" title={text1}></Tooltip> */}
-                <h1>Quote</h1>
+                <h1 className="text-gray-100">Quote</h1>
               </Link>
             </li>
           </ul>
